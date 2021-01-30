8c2c54681c6e2f457d77ceaecd34a660a52fa3ac
michael allen <michael@allennetworks.com>
2021-01-30 11:17:06 -0800

    Added a print line

--- src/smith/josh/Main.java
+++ src/smith/josh/Main.java
@@ -3,5 +3,7 @@ package smith.josh;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Josh");
+        System.out.println("Hello Josh, my name is Michael");
     }
+
 }
